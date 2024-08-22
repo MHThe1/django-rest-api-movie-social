945c007ffb8011c4062cca508b1e3449aa85e563
Mehedi Hasan <mehedihtanvir@gmail.com>
2024-08-23 01:38:37 +0600

    movie model created and returned as a JsonResponse

--- moviemagic_app/views.py
+++ moviemagic_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from moviemagic_app.models import Movie
+from django.http import JsonResponse
 
 # Create your views here.
+def movie_list(request):
+    movies = Movie.objects.all()
+    data = {'movies': list(movies.values()) }
+
+    return JsonResponse(data)
