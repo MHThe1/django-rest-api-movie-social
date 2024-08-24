8c5a8407e5bd645d2f8b725198c1ac23b50b0cae
Mehedi Hasan <mehedihtanvir@gmail.com>
2024-08-24 23:32:24 +0600

    using class based views and generic views

--- moviemagic_app/api/urls.py
+++ moviemagic_app/api/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from moviemagic_app.api.views import WatchListAV, WatchListDetailsAV, StreamPlatformListAV, StreamPlatformDetailsAV
+from moviemagic_app.api.views import (WatchListAV, WatchListDetailsAV, 
+                                      StreamPlatformListAV, StreamPlatformDetailsAV, 
+                                      ReviewList, ReviewDetail)
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='watch-list'),
     path('<int:pk>', WatchListDetailsAV.as_view(), name='watch-details'),
     path('platformlist/', StreamPlatformListAV.as_view(), name='streaming-list'),
     path('platform/<int:pk>', StreamPlatformDetailsAV.as_view(), name='platform-details'),
+    path('review', ReviewList.as_view(), name='review-list'),
+    path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
 
 ]
--- moviemagic_app/api/views.py
+++ moviemagic_app/api/views.py
@@ -1,10 +1,41 @@
-# from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from moviemagic_app.models import WatchList, StreamPlatform
-from moviemagic_app.api.serializers import WatchListSerializer, StreamPlatformSerializer
+from rest_framework import mixins
+from rest_framework import generics
 
+from moviemagic_app.models import WatchList, StreamPlatform, Review
+from moviemagic_app.api.serializers import (WatchListSerializer, StreamPlatformSerializer, 
+                                            ReviewSerializer)
+
+class ReviewList(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    
+
+# class ReviewDetail(mixins.RetrieveModelMixin, generics.GenericAPIView):
+#     queryset = Review.objects.all()
+#     serializer_class = ReviewSerializer
+    
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+
+# class ReviewList(mixins.ListModelMixin,
+#                   mixins.CreateModelMixin,
+#                   generics.GenericAPIView):
+#     queryset = Review.objects.all()
+#     serializer_class = ReviewSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+    
 
 class WatchListAV(APIView):
     
